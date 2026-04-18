b9b3f3f2d0e481b45c038d97231e2633bfadaf87
dontwanttothink <barquean.09excavacion@icloud.com>
2026-04-17 20:22:14 -0500

    Add functionality

---
+++ src/lib/components/Riesgo.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    const { given } = $props();
+    const [_, dropout] = $derived(given);
+</script>
+
+<p>
+    La probabilidad de deserción se estima en <strong
+        >{Math.round(dropout * 100)}%</strong
+    >.
+</p>
+
+{#if dropout > 0.8}
+    <p class="high">El riesgo es alto.</p>
+{:else if dropout > 0.5}
+    <p class="moderate">El riesgo es moderado.</p>
+{:else}
+    <p class="low">El riesgo es bajo.</p>
+{/if}
--- src/routes/predecir/+page.svelte
+++ src/routes/predecir/+page.svelte
@@ -1,5 +1,128 @@
 <script lang="ts">
+    import Riesgo from "$lib/components/Riesgo.svelte";
     import { fade } from "svelte/transition";
+
+    let socioeconómica = $state(25);
+    let papa = $state(3.0);
+    let avance = $state(40);
+    let apoyoAlimentario = $state("1");
+    let admisiónEspecial = $state("0");
+    let sanciónAcadémica = $state("1");
 </script>
 
-<h2 transition:fade={{ duration: 150 }}>Características del estudiante</h2>
+<div transition:fade={{ duration: 150 }} id="container">
+    <p>
+        Introduzca las variables independientes que desee y vea cómo afectan la
+        estimación de riesgo del modelo.
+    </p>
+
+    <h2>Características del estudiante</h2>
+    <form>
+        <label for="selección-socioeconómica">PBM socioeconómico</label>
+        <input
+            type="number"
+            id="selección-calificación"
+            name="calificación"
+            min="0"
+            max="100"
+            bind:value={socioeconómica}
+        />
+
+        <label for="selección-papa">PAPA acumulado</label>
+        <input
+            type="number"
+            id="selección-papa"
+            name="papa"
+            min="0"
+            max="5"
+            step="0.1"
+            bind:value={papa}
+        />
+
+        <label for="selección-avance"
+            >Avance en la carrera, como porcentaje</label
+        >
+        <input
+            type="number"
+            id="selección-avance"
+            name="avance"
+            min="0"
+            max="100"
+            bind:value={avance}
+        />
+
+        <label for="selección-apoyo-alimentario"
+            >¿Recibe apoyo alimentario?</label
+        >
+        <select
+            id="selección-apoyo-alimentario"
+            name="apoyo-alimentario"
+            bind:value={apoyoAlimentario}
+        >
+            <option value="0">No</option>
+            <option value="1">Sí</option>
+        </select>
+
+        <label for="selección-admisión-especial"
+            >¿Recibió admisión especial?</label
+        >
+        <select
+            id="selección-admisión-especial"
+            name="admisión-especial"
+            bind:value={admisiónEspecial}
+        >
+            <option value="0">No</option>
+            <option value="1">Sí</option>
+        </select>
+
+        <label for="selección-sanción-académica"
+            >¿Recibió una sanción académica?</label
+        >
+        <select
+            id="selección-sanción-académica"
+            name="sanción-académica"
+            bind:value={sanciónAcadémica}
+        >
+            <option value="0">No</option>
+            <option value="1">Sí</option>
+        </select>
+    </form>
+
+    <h2>Resultados</h2>
+
+    {#await import("$lib/modelo") then m}
+        <Riesgo
+            given={m.default([
+                socioeconómica,
+                papa,
+                avance,
+                Number(apoyoAlimentario),
+                Number(admisiónEspecial),
+                Number(sanciónAcadémica),
+            ])}
+        ></Riesgo>
+    {/await}
+</div>
+
+<style>
+    label,
+    select,
+    input {
+        display: flex;
+    }
+
+    select,
+    input {
+        appearance: base-select;
+        margin-bottom: 1rem;
+    }
+
+    label {
+        margin-bottom: 0.5rem;
+    }
+
+    #container {
+        max-width: 500px;
+        margin: 0 auto;
+    }
+</style>
