c390df61075f8a5afcb5239b345af987040589f3
dontwanttothink <barquean.09excavacion@icloud.com>
2026-04-21 21:56:08 -0500

    Imagen de 'en construcción'

---
+++ src/routes/estimar-eficiencia/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import trabajo1 from "$lib/assets/RESULTADOS/EN TRABAJO 1.jpeg";
+    import trabajo2 from "$lib/assets/RESULTADOS/EN TRABAJO 2.jpeg";
+    import { fade } from "svelte/transition";
+</script>
+
+<div transition:fade={{ duration: 150 }}>
+    <img alt="Perrito trabajando" src={trabajo1} />
+    <img alt="Miquito trabajando" src={trabajo2} />
+</div>
+
+<style>
+    div {
+        display: flex;
+        gap: 1rem;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img {
+        display: block;
+        border-radius: 1rem;
+        max-width: 300px;
+    }
+</style>
